695610f18798b2f74580e34fa782edcb51fe94b9
Jon Bristow <devnull@localhost>
2010-07-19 19:40:26 -0700

    euler30

--- euler030.rb
+++ euler030.rb
@@ -1,6 +1,8 @@
 def solver
   sum=0
-  4000.upto(200000) do |i|
+  min=2
+  max=(9**5)*6
+  min.upto(max) do |i|
     digits=i.to_s.split(//).map{|e|e.to_i**5}.inject(:+)
     sum+=digits if digits==i
   end
